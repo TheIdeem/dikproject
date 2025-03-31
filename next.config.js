4a08601043ee80b0714a6598d495b88ab7017972
TheIdeem <ulg.mehdi@gmail.com>
2025-03-31 23:08:13 +0400

    Add ESLint configuration and ignore files for improved build process
    - Created a new `.eslintignore` file to exclude build artifacts, dependencies, and miscellaneous files from linting.
    - Updated `.eslintrc.json` to disable specific ESLint rules for a more lenient linting process.
    - Modified `next.config.js` to ignore type checking and ESLint during production builds for faster build times.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Disable type checking during production build for faster builds
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  // Disable ESLint during production build for faster builds
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig 
